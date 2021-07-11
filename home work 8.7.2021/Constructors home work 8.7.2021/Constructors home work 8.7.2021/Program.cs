60332f994fa1e27f0536f94c0fe6f811041d06ae
daniel shlaim <dada373@gmail.com>
2021-07-11 20:00:22 +0300

    upgrade 11.7.2021

---
+++ home work 8.7.2021/Constructors home work 8.7.2021/Constructors home work 8.7.2021/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_home_work_8._7._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Ex 7
+            Car c1 = new Car("Honda");
+            c1.GetBrand();
+            #endregion
+            #region Ex8
+            ConsolePlus console_Plus = new ConsolePlus("Daniel",26,1995);
+            #endregion
+            #region Ex 9
+            Console.WriteLine("Enter size of Array");
+            int size = int.Parse(Console.ReadLine());
+            string[] myarray = new string[size];
+            int[] myarray1 = new int[size];
+            Random Agerandom = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("Enter Name");
+                person myperson = new person(Console.ReadLine());
+                myarray[i] = myperson._Name;
+                
+                person myperson2 = new person(myarray[i], Agerandom.Next(1,101));
+                myarray1[i] = myperson2._age;
+            }
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"name: {myarray[i]}\nage {myarray1[i]}");
+
+            }
+            #endregion
+        }
+
+    }
+    class Car
+    {
+        public string _Brand; // שם מותג הרכב
+        public Car(string brand) // בנאי שמביא את מותג הרכב
+        {
+            _Brand = brand;
+        }
+        public  void GetBrand() // פונקציה שמדפיסה את שם המותג של הרכב
+        {
+            Console.WriteLine(_Brand);
+        }
+    }
+    class ConsolePlus
+    {
+        public ConsolePlus()
+        {
+            Console.WriteLine("Default");
+        }
+        public ConsolePlus(string First) : this()
+        {
+            Console.WriteLine(First);
+        }
+        public ConsolePlus( string First,int num1 , int num2 ) : this(First)
+        {
+            Console.WriteLine(num1 + num2);
+        }
+    }
+    class person
+    {
+        public int _age;
+        public string _Name;
+        public person(string Name)
+        {
+            _Name = Name;
+        }
+        public person(string Name, int age) : this(Name)
+        {
+            _age = age;
+        }
+    }
+    
+
+}
---
+++ home work 8.7.2021/home work 8.7.2021/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home_work_8._7._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Ex8 computer
+            //Computer c1 = new Computer();
+            //c1.TellMePrice(500);
+            //c1.TellMeScreenSize(60);
+            //c1.AddProcessor(5);
+            //c1._model = "lenovo";
+            //c1.TurnOn();
+            //c1.TurnOff();
+            //Console.WriteLine(c1.OverRide());
+
+            //Computer c2 = new Computer();
+            //c2.TellMePrice(1000);
+            //c2.TellMeScreenSize(90);
+            //c2._model = "Apple";
+            //c2.AddProcessor(10);
+            //c2.TurnOn();
+
+
+            //Computer c3 = new Computer();
+            //c3._model = "Samsung";
+            //c3.TellMeScreenSize(70);
+            //c3.AddProcessor(8);
+            //c3.TellMePrice(899);
+            //c3.TurnOn();
+            //c3.TurnOff();
+            #endregion
+            #region Ex9 Table
+            //Table T1 = new Table();  // בנאי ריק הגדרה רגילה ,מגדירים ערכים מחוץ
+            //T1._NumberOfLegs = 4;
+            //T1._ColorOfTable = "brown";
+            //T1._Type = "Alon";
+            //Table T2 = new Table(4,"brown","Alon"); // בנאי סטנדרטי מגדירים ערכים מראש
+            //Table T3 = new Table //  בנאי מקבל ערכים על פי הוראות יצרן
+            //{
+            //    _NumberOfLegs = 4,
+            //    _ColorOfTable = "brown",
+            //    _Type = "Alon"
+            //};
+            //Table T4 = T2;
+
+            #endregion
+            #region Ex 10
+            Octagon oc1 = new Octagon(10, 5);
+
+            #endregion
+        }
+
+    }
+    class Computer
+    {
+        public string _model;
+        public int _price;
+        public int _numberofprocessors;
+        public float _screensize;
+        public bool _isturnOn;
+
+
+        public  void TellMePrice(int price)
+        {
+            _price = price;
+        }
+
+        public void TellMeScreenSize(float screenSize)
+        {
+            _screensize = screenSize;
+        }
+
+        public void TurnOn()
+        {
+            _isturnOn = true;
+        }
+
+        public void TurnOff()
+        {
+            _isturnOn = false;
+        }
+
+        public string OverRide()
+        {
+            return ToString();
+        }
+
+        public void AddProcessor(int Processors)
+        {
+            _numberofprocessors = Processors; 
+        }
+        
+    }
+    class Table
+    {
+        public int _NumberOfLegs;
+        public string _ColorOfTable;
+        public string _Type;
+
+        public Table() // T1,T3
+        {
+           
+        }
+
+        public Table(int numberoflegs, string color,string type) //T2
+        {
+            _NumberOfLegs = numberoflegs;
+            _ColorOfTable = color;
+            _Type = type;
+        }
+       
+       
+
+        
+    }
+
+    class Octagon
+    {
+        public double _Area;
+        public double _Scoop;
+        public Octagon(int high , int width)
+        {
+            _Area = GetArea(high, width);
+            _Scoop = GetScoop(high, width);
+        }
+        public double GetArea(int high, int width) // חישוב שטח
+        {
+            return high * width;
+        }
+        public double GetScoop(int high , int width) // חישוב היקף
+        {
+            return (high * 2) +( width * 2);
+        }
+    }
+
+
+    
+}
